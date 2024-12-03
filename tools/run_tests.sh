27f82605fd1638a90d434a2419979b21f8a1845e
Andrew Sazonov <andrew.sazonov@icloud.com>
2024-12-03 15:46:05 +0100

    Clean up

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -4,6 +4,3 @@ echo "PYTHONPATH: ${PYTHONPATH}"
 
 echo "\033[0;33m:::::: Run unit tests\033[0m"
 pytest tests/unit_tests/ --color=yes --disable-warnings
-
-echo "\033[0;33m:::::: Run integration tests\033[0m"
-pytest tests/integration_tests/ --color=yes --disable-warnings
