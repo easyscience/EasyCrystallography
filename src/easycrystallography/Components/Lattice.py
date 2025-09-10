9bd4a87bf7486edaa664551c22c6eaf68d12c442
rozyczko <piotr.rozyczko@gmail.com>
2025-09-10 09:12:33 +0200

    cleanup. fixed dependency forms

--- src/easycrystallography/Components/Lattice.py
+++ src/easycrystallography/Components/Lattice.py
@@ -23,7 +23,6 @@ from typing import Union
 
 import numpy as np
 
-# from easyscience.Constraints import ObjConstraint
 from easyscience import ObjBase as BaseObj
 from easyscience.utils.decorators import memoized
 from easyscience.variable import Parameter
@@ -853,13 +852,13 @@ class PeriodicLattice(Lattice):
         )
 
         # Go through the cell systems
+        dependency_expression_a = 'a'
+        dependency_expression_alpha = 'alpha'
+        dependency_map_a = {'a': 'self.length_a'}
+        dependency_map_alpha = {'alpha': 'self.angle_alpha'}
         if crys_system == 'cubic':
-            # self.length_a.user_constraints['sg_1'] = ObjConstraint(self.length_b, '', self.length_a)
-            # self.length_a.user_constraints['sg_1']()
-            # self.length_a.user_constraints['sg_2'] = ObjConstraint(self.length_c, '', self.length_a)
-            # self.length_a.user_constraints['sg_2']()
-            self.length_b.make_dependent_on(dependency_expression='self.length_a')
-            self.length_c.make_dependent_on(dependency_expression='self.length_a')
+            self.length_b.make_dependent_on(dependency_expression=dependency_expression_a, dependency_map=dependency_map_a)
+            self.length_c.make_dependent_on(dependency_expression=dependency_expression_a, dependency_map=dependency_map_a)
             self.angle_alpha = 90
             self.angle_alpha.enabled = False
             self.angle_beta = 90
@@ -867,9 +866,7 @@ class PeriodicLattice(Lattice):
             self.angle_gamma = 90
             self.angle_gamma.enabled = False
         elif crys_system == 'hexagonal' or trig_test:
-            # self.length_a.user_constraints['sg_1'] = ObjConstraint(self.length_b, '', self.length_a)
-            # self.length_a.user_constraints['sg_1']()
-            self.length_b.make_dependent_on(dependency_expression='self.length_a')
+            self.length_b.make_dependent_on(dependency_expression=dependency_expression_a, dependency_map=dependency_map_a)
             self.angle_alpha = 90
             self.angle_alpha.enabled = False
             self.angle_beta = 90
@@ -877,22 +874,12 @@ class PeriodicLattice(Lattice):
             self.angle_gamma = 120
             self.angle_gamma.enabled = False
         elif crys_system == 'trigonal' and not trig_test:
-            # self.length_a.user_constraints['sg_1'] = ObjConstraint(self.length_b, '', self.length_a)
-            # self.length_a.user_constraints['sg_1']()
-            # self.length_a.user_constraints['sg_2'] = ObjConstraint(self.length_c, '', self.length_a)
-            # self.length_a.user_constraints['sg_2']()
-            # self.angle_alpha.user_constraints['sg_1'] = ObjConstraint(self.angle_beta, '', self.angle_alpha)
-            # self.angle_alpha.user_constraints['sg_1']()
-            # self.angle_alpha.user_constraints['sg_2'] = ObjConstraint(self.angle_gamma, '', self.angle_alpha)
-            # self.angle_alpha.user_constraints['sg_2']()
-            self.length_b.make_dependent_on(dependency_expression='self.length_a')
-            self.length_c.make_dependent_on(dependency_expression='self.length_a')
-            self.angle_beta.make_dependent_on(dependency_expression='self.angle_alpha')
-            self.angle_gamma.make_dependent_on(dependency_expression='self.angle_alpha')
+            self.length_b.make_dependent_on(dependency_expression=dependency_expression_a, dependency_map=dependency_map_a)
+            self.length_c.make_dependent_on(dependency_expression=dependency_expression_a, dependency_map=dependency_map_a)
+            self.angle_beta.make_dependent_on(dependency_expression=dependency_expression_alpha, dependency_map=dependency_map_alpha)
+            self.angle_gamma.make_dependent_on(dependency_expression=dependency_expression_alpha, dependency_map=dependency_map_alpha)
         elif crys_system == 'tetragonal':
-            # self.length_a.user_constraints['sg_1'] = ObjConstraint(self.length_b, '', self.length_a)
-            # self.length_a.user_constraints['sg_1']()
-            self.length_b.make_dependent_on(dependency_expression='self.length_a')
+            self.length_b.make_dependent_on(dependency_expression=dependency_expression_a, dependency_map=dependency_map_a)
             self.angle_alpha = 90
             self.angle_alpha.enabled = False
             self.angle_beta = 90
